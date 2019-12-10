8006a6f7c8aa9244be0a7bffa080c81ea7cbcab4
lucas-barbero <lucas.barbero@hotmail.fr>
2019-12-10 10:20:18 +0100

    Nouvelle lib autocomplete

--- js/main.js
+++ js/main.js
@@ -14,28 +14,49 @@ $(document).ready(function () {
     "PREFIX dbr: <http://dbpedia.org/resource/>",
     "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
 
-    "select distinct ?name where {",
-
+    "select distinct ?name ?query where {",
     "dbr:Jazz dbo:musicSubgenre ?query.",
     "?query foaf:name ?name.",
-
-
     "} LIMIT 100000",
   ].join(" ");
 
   sparqlQuery(query).then(function (data) {
-      subGenre = data.results.bindings.map(x => x.name.value); // TODO a voir si on get pas directement uri + nom pour pas avoir a faire deux requetes aiu moment du search + keylistener + genre l'autocompletion en fonction du type de recherche
+      console.log(data);
+
+      subGenre = data.results.bindings.map(x => {
+        return {name: x.name.value, value: x.query.value};
+      });
       console.log(subGenre);
       initAutoComplete(subGenre)
     }
   );
 });
 
+function initAutoComplete(data) {
+  $('input').typeahead({
+
+    source: data,
+    
+    // how many items to display
+    items: 5,
+
+    // equalize the dropdown width
+    alignWidth: true,
+
+    // typeahead dropdown template
+    menu: '<ul class="typeahead dropdown-menu"></ul>',
+    item: '<li><a href="#"></a></li>',
+    // auto select
+    autoSelect: true,
+
+  });
+}
+
 
 function sparqlQuery(query) {
   return new Promise(function (resolve, reject) {
 
-    var url = "http://dbpedia.org/sparql";
+    let url = "http://dbpedia.org/sparql";
     const queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
     console.log(queryUrl);
     $.ajax({
@@ -49,29 +70,5 @@ function sparqlQuery(query) {
         reject(err)
       }
     });
-  });
-}
-
-function initAutoComplete(data) {
-  $('input').typeahead({
-
-    source: data,
-    // how many items to display
-    items: 5,
-
-    // equalize the dropdown width
-    alignWidth: true,
-
-    // typeahead dropdown template
-    menu: '<ul class="typeahead dropdown-menu"></ul>',
-    item: '<li><a href="#"></a></li>',
-    // auto select
-    autoSelect: true,
-
-    // callback
-    onSelect: function () {
-      console.log("coucou")
-    },
-
   });
 }
