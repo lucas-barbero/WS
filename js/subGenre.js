b41e82d67e8a15fa7b2d831331d3b0166c2d4203
FelixFonteneau <felix.fonteneau@hotmail.com>
2019-12-10 10:47:21 +0100

    sous genre avec nom

--- js/subGenre.js
+++ js/subGenre.js
@@ -7,20 +7,43 @@ $(document).ready(function () {
     // TODO PAGE INVALIDE signaler une erreur et faire un lien pour rediriger vers la page principale
 
   }
+  getName(subGenre);
 
-  var query = [
-    "PREFIX dbo: <http://dbpedia.org/ontology/>",
-    "PREFIX dbr: <http://dbpedia.org/resource/>",
-    "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
-
-    "select distinct ?name where {",
-
-    "dbr:Jazz dbo:musicSubgenre ?query.",
-    "dbr:"+subGenre+" foaf:name ?name.",
-
-
-    "} LIMIT 100000",
-  ].join(" ");
 
 
 });
+
+function getName(subGenre) {
+  return new Promise(function (resolve, reject) {
+    var query = [
+      "PREFIX dbo: <http://dbpedia.org/ontology/>",
+      "PREFIX dbr: <http://dbpedia.org/resource/>",
+      "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
+
+      "select distinct ?name where {",
+      "dbr:"+subGenre+" foaf:name ?name.",
+      "FILTER(langMatches(lang(?name), \"EN\")).\n",
+      "}",
+    ].join(" ");
+    var url = "http://dbpedia.org/sparql";
+    const queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
+    $.ajax({
+      dataType: "json",
+      url: queryUrl,
+      success: function (data) {
+        //console.log(data);
+        console.log(data.results);
+        console.log(data.results.bindings[0].name.value);
+        showName(data.results.bindings[0].name.value);
+      },
+      error: function (err) {
+        reject(err)
+      }
+    });
+  });
+}
+
+function showName(name){
+  document.getElementById("name").innerHTML = name;
+
+}
